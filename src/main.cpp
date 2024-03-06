12691ec3b76155a6599f7e63ecd1b1df8ccdb70d
Allan Gonçalves da Silva <allan.goncalves@elements.com.br>
2024-03-06 16:25:03 -0300

    Pagina sem acoes externas apenas design botões de horarios e liga e desliga

--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,16 @@
 #include <Arduino.h>
 #include <ESPAsyncWebServer.h>
-#include <NTPClient.h>
-#include <TimeLib.h>
 #include <WiFi.h>
+#include <NTPClient.h>
 #include <WiFiUdp.h>
-#include <AsyncWebSocket.h>
-
-#define PWM_OUT_PIN 32
-#define PWM_OUT_PIN_2 33
-
-
-AsyncWebServer server(80);
-AsyncWebSocket ws("/ws");
 
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", -3 * 3600, 30000);
+NTPClient timeClient(ntpUDP, "pool.ntp.org", -3 * 60 * 60);  // Ajuste para o fuso horário de Brasília (GMT-3)
 
-int temperatura = 0;
-int tempAuto = 0;
-int atualTemp;
-bool controlAuto = false;
-
-const char *mainSSID = "Elements";
-const char *mainPassword = "Elements@@2024!";
-
-void notifyClients() {
-  ws.textAll(String(tempAuto));
-}
-
-void updateTemperatureBox() {
-  notifyClients();
-}
-
-void handleIncreaseTemperature(AsyncWebServerRequest *request) {
-  if (temperatura < 100) {
-    temperatura += 10;
-    updateTemperatureBox();
-  }
-  Serial.println("Temperatura atual: " + String(temperatura));
-  request->send(200, "text/plain", "");
-}
-
-void handleDecreaseTemperature(AsyncWebServerRequest *request) {
-  if (temperatura > 0) {
-    temperatura -= 10;
-    updateTemperatureBox();
-  }
-  Serial.println("Temperatura atual: " + String(temperatura));
-  request->send(200, "text/plain", "");
-}
-
-
-
-void handleAutoTemperature() {
-  if (tempAuto == 0) {
-    tempAuto = 100;
-  } else {
-    tempAuto = 0;
-  }
-  Serial.println("TempAuto atual: " + String(tempAuto));
-  updateTemperatureBox();
-}
-
-void handleCicloCircadiano(AsyncWebServerRequest *request) {
-  controlAuto = !controlAuto;
-  Serial.println("Ciclo Circadiano ativado: " + String(controlAuto));
-  
-  String response = (controlAuto) ? "true" : "false";
-  request->send(200, "text/plain", response);
-}
+AsyncWebServer server(80);
 
 void connectToWiFi() {
-  WiFi.begin(mainSSID, mainPassword);
+  WiFi.begin("Elements", "Elements@@2024!");
   int attempt = 0;
 
   while (WiFi.status() != WL_CONNECTED && attempt < 10) {
@@ -90,81 +29,88 @@ void connectToWiFi() {
 
 void setup() {
   WiFi.setHostname("luminaria-elements");
-  ledcSetup(0, 5000, 10);
-  ledcSetup(1, 5000, 10);
-  ledcAttachPin(PWM_OUT_PIN, 0);
-  ledcAttachPin(PWM_OUT_PIN_2, 1);
-
-
-
   Serial.begin(115200);
 
-  // Desconectar de qualquer rede anterior
   WiFi.disconnect(true);
-
   connectToWiFi();
 
-  ws.onEvent([](AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len) {
-    // WebSocket message handling (if needed)
-  });
+  // Inicialize o cliente NTP
+  timeClient.begin();
 
-  server.addHandler(&ws);
-
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    // Código HTML com os botões e a imagem abaixo
     String html = "<html><head><meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'>";
-    html += "<style>body {font-family: Arial, sans-serif; background-color: #f4f4f4; margin: 0;}";
-    html += ".container {width: 80%; margin: auto; text-align: center;}";
-    html += "h2 {text-align: center; color: #333;}";
-    html += "button {font-size: 20px; margin: 10px; padding: 10px 20px; width: 150px; cursor: pointer; border: none; border-radius: 5px;}";
-    html += "#quenteBtn {background-color: #FFD700; color: #333;}";
-    html += "#frioBtn {background-color: #ADD8E6; color: #333;}";
-    html += "#cicloBtn {font-size: 16px; margin: 10px; padding: 10px 20px; width: 150px; cursor: pointer; border: none; border-radius: 5px; background-color: ";
-    html += (controlAuto) ? "#800080;" : "#808080; color: #fff;}";
-    html += ".quente-info, .frio-info, .temp-auto-info {margin: 10px; padding: 10px; width: 150px; display: inline-block;}";
-    html += ".quente-info {background-color: #FFD700;}";
-    html += ".frio-info {background-color: #ADD8E6;}";
-    html += ".temp-auto-info {background-color: #8A2BE2;}";
-    html += "</style></head><body><div class='container'>";
-    html += "<h2>Lumin&aacute;ria Ventura</h2>";
-    html += "<button onclick='decreaseTemperature()' id='quenteBtn' " + String((controlAuto) ? "disabled" : "") + ">Quente</button>";
-    html += "<button onclick='increaseTemperature()' id='frioBtn' " + String((controlAuto) ? "disabled" : "") + ">Frio</button>";
-
-    //html += "<div class='quente-info' id='quenteInfo'>Quente (" + String(100 - temperatura) + "%)</div>";
-    //html += "<div class='frio-info' id='frioInfo'>Frio (" + String(temperatura) + "%)</div>";
-    html += "<button onclick='toggleCiclo()' id='cicloBtn'>Ciclo Circadiano</button>";
+    html += "<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>";
+    html += "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css'>";
+    html += "<style>body {font-family: Arial, sans-serif; background-color: #ffffff; margin: 0; padding: 0; position: relative;}";
+    html += ".container {width: 90%; margin: auto; text-align: center;}";
+    html += ".header {padding: 5px 0; margin-bottom: 5px; text-align: center;}";
+    html += ".logo {width: 50px; height: 50px; cursor: pointer; margin-bottom: 25px; }";
+    html += "img {width: 100%; height: auto; max-width: 80%; margin: 20px auto; display: block;}";
+    html += ".switch-container {display: flex; flex-direction: column; align-items: center;}";
+    html += ".switch-label {margin-bottom: 10px; font-size: 16px;}";
+    html += ".switch {position: relative; display: inline-block; width: 60px; height: 34px;}";
+    html += ".switch input {display: none;}";
+    html += ".slider {position: absolute; cursor: pointer; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; -webkit-transition: .4s; transition: .4s;}";
+    html += ".slider:before {position: absolute; content: ''; height: 26px; width: 26px; left: 4px; bottom: 4px; background-color: white; -webkit-transition: .4s; transition: .4s;}";
+    html += "input:checked + .slider {background-color: #2196F3;}";
+    html += "input:focus + .slider {box-shadow: 0 0 1px #2196F3;}";
+    html += "input:checked + .slider:before {-webkit-transform: translateX(26px); -ms-transform: translateX(26px); transform: translateX(26px);}";
+    html += ".slider.round {border-radius: 34px;}";
+    html += ".slider.round:before {border-radius: 50%;}";
+    html += ".button-container {margin-top: 20px; display: flex; flex-wrap: wrap; justify-content: center;}";
+    html += ".button-container h2 {width: 100%; text-align: center; margin-bottom: 10px; font-size: 18px;}";
+    html += ".button-container button {margin: 5px; padding: 10px; font-size: 16px; flex: 0 0 calc(33.333% - 10px); max-width: calc(33.333% - 10px);}";
+    html += ".responsive-image {width: 100%; height: auto; max-width: 80%; margin: 20px auto; display: block;}";
+    html += ".NT-footer-icones {background-color: #000; color: #fff; padding: 20px 0; text-align: center;}";
+    html += "</style></head><body>";
+    html += "<div class='header'><img class='logo' src='https://elements.com.br/cdn/shop/files/Group_1.png?v=1702562143&width=50' onclick='window.location.href=\"https://elements.com.br/\"' alt='Logo'></div>";
+    html += "<div class='container'>";
+    html += "<h2 style='font-size: 24px;'>Luminária Solare</h2>";
+    html += "<div class='switch-container'>";
+    html += "<label class='switch-label' style='font-size: 16px;'>ON/OFF</label>";
+    html += "<label class='switch'><input type='checkbox'><span class='slider round'></span></label>";
+    html += "<label class='switch-label' style='font-size: 16px;'>Modo Automático</label>";
+    html += "<label class='switch'><input type='checkbox'><span class='slider round'></span></label>";
+    html += "</div>";
+    html += "<div class='button-container'>";
+    html += "<h2>Escolha a luz do horário que deseja:</h2>";
+    html += "<button onclick='setLight(8)'>8:00hrs</button>";
+    html += "<button onclick='setLight(10)'>10:00hrs</button>";
+    html += "<button onclick='setLight(12)'>12:00hrs</button>";
+    html += "<button onclick='setLight(14)'>14:00hrs</button>";
+    html += "<button onclick='setLight(16)'>16:00hrs</button>";
+    html += "<button onclick='setLight(18)'>18:00hrs</button>";
+    html += "</div>";
+    html += "<img class='responsive-image' src='https://dcuatro.com/wp-content/uploads/2020/08/Articulo_alejandro_imagen.jpg' alt='Imagem'>";
+    html += "</div>";
+    html += "<div class='NT-footer-icones'>";
+    // Restante do código para os ícones
+    html += "</div>";
     html += "<script>";
-    html += "function decreaseTemperature() { fetch('/decreaseTemperature'); }";
-    html += "function increaseTemperature() { fetch('/increaseTemperature'); }";
-    html += "function toggleCiclo() { fetch('/toggleCiclo').then(() => location.reload()); }";
+    html += "function setLight(hour) {";
+    html += "console.log('Botão pressionado para o horário:', hour);";
+    html += "}";
     html += "</script>";
-    html += "</div></body></html>";
+    html += "</body></html>";
     request->send(200, "text/html", html);
-  });
+});
 
-  server.on("/decreaseTemperature", HTTP_GET, handleDecreaseTemperature);
-  server.on("/increaseTemperature", HTTP_GET, handleIncreaseTemperature);
-  server.on("/toggleCiclo", HTTP_GET, handleCicloCircadiano);
+
+
+  // Adicione o endpoint para obter o estado atual
+  server.on("/getState", HTTP_GET, [](AsyncWebServerRequest *request) {
+    String state = "{\"time\":\"" + timeClient.getFormattedTime() + "\"}";
+    request->send(200, "application/json", state);
+  });
 
   server.begin();
 }
 
 void loop() {
-  atualTemp = (controlAuto) ? tempAuto : temperatura;
+  // Atualize o cliente NTP
   timeClient.update();
-  int currentHour = timeClient.getHours();
-  int currentMinute = timeClient.getMinutes();
-  int currentSecond = timeClient.getSeconds();
 
-  tempAuto = map(currentSecond,0,60,0,100);
-  if(controlAuto == false) atualTemp = temperatura;
-  else atualTemp = tempAuto;
-
-  int pwmValue = map(atualTemp, 0, 100, 0, 255);
-  int pwmValue2 = map(atualTemp, 100, 0, 0, 255);
-
-  ledcWrite(0, pwmValue);
-  ledcWrite(1, pwmValue2);
-  notifyClients();
-
+  // Adicione a lógica do ciclo circadiano aqui
+  // Adicione outras lógicas necessárias para o ciclo circadiano
 }
-
