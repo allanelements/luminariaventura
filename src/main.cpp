84288297927fe08827119bf954c824cd71af11b6
Allan Gonçalves da Silva <allan.goncalves@elements.com.br>
2024-03-25 14:18:32 -0300

    Botões alterando as variaveis

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,29 @@
+
 #include <Arduino.h>
 #include <ESPAsyncWebServer.h>
 #include <WiFi.h>
 #include <NTPClient.h>
 #include <WiFiUdp.h>
+#include <ArduinoJson.h>
+
+#define PWM_OUT_PIN 32
+#define PWM_OUT_PIN_2 33
+
+int currentHour;
+int currentMinute;
+int currentSecond;
+
+int pwmValue = 0;
+int pwmValue2 = 0;
+bool onOffState = 1;
+bool modoAutomaticoState = 1;
+int selectedHour = 0;
 
 WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP, "pool.ntp.org", -3 * 60 * 60);  // Ajuste para o fuso horário de Brasília (GMT-3)
+NTPClient timeClient(ntpUDP, "pool.ntp.org", -3 * 60 * 60);
 
 AsyncWebServer server(80);
+AsyncWebSocket ws("/ws");
 
 void connectToWiFi() {
   WiFi.begin("Elements", "Elements@@2024!");
@@ -27,18 +43,42 @@ void connectToWiFi() {
   }
 }
 
-void setup() {
-  WiFi.setHostname("luminaria-elements");
-  Serial.begin(115200);
+void actionLed() {
+  timeClient.update();
+  currentHour = timeClient.getHours();
+  currentMinute = timeClient.getMinutes();
+  currentSecond = timeClient.getSeconds();
+  ledcWrite(0, pwmValue);
+  ledcWrite(1, pwmValue2);
+}
 
-  WiFi.disconnect(true);
-  connectToWiFi();
+void handleWebSocketMessage(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t * data, size_t len) {
+  DynamicJsonDocument jsonDoc(256);
+  deserializeJson(jsonDoc, data, len);
 
-  // Inicialize o cliente NTP
-  timeClient.begin();
+  Serial.println("Mensagem WebSocket recebida:");
 
-server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
-    // Código HTML com os botões e a imagem abaixo
+  if (jsonDoc.containsKey("onOffState")) {
+    onOffState = jsonDoc["onOffState"];
+    Serial.print("onOffState atualizado para: ");
+    Serial.println(onOffState);
+  }
+
+  if (jsonDoc.containsKey("modoAutomaticoState")) {
+    modoAutomaticoState = jsonDoc["modoAutomaticoState"];
+    Serial.print("modoAutomaticoState atualizado para: ");
+    Serial.println(modoAutomaticoState);
+  }
+
+  if (jsonDoc.containsKey("selectedHour")) {
+    selectedHour = jsonDoc["selectedHour"];
+    Serial.print("selectedHour atualizado para: ");
+    Serial.println(selectedHour);
+  }
+}
+
+
+void handleRoot(AsyncWebServerRequest *request) {
     String html = "<html><head><meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'>";
     html += "<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>";
     html += "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css'>";
@@ -68,10 +108,13 @@ server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
     html += "<div class='container'>";
     html += "<h2 style='font-size: 24px;'>Luminária Solare</h2>";
     html += "<div class='switch-container'>";
+
     html += "<label class='switch-label' style='font-size: 16px;'>ON/OFF</label>";
-    html += "<label class='switch'><input type='checkbox'><span class='slider round'></span></label>";
+    html += "<label class='switch'><input id='onOffSwitch' type='checkbox' onchange='toggleOnOff(this)'><span class='slider round'></span></label>";
     html += "<label class='switch-label' style='font-size: 16px;'>Modo Automático</label>";
-    html += "<label class='switch'><input type='checkbox'><span class='slider round'></span></label>";
+    html += "<label class='switch'><input id='modoAutomaticoSwitch' type='checkbox' onchange='toggleModoAutomatico(this)'><span class='slider round'></span></label>";
+
+    
     html += "</div>";
     html += "<div class='button-container'>";
     html += "<h2>Escolha a luz do horário que deseja:</h2>";
@@ -87,30 +130,118 @@ server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
     html += "<div class='NT-footer-icones'>";
     // Restante do código para os ícones
     html += "</div>";
-    html += "<script>";
-    html += "function setLight(hour) {";
-    html += "console.log('Botão pressionado para o horário:', hour);";
-    html += "}";
-    html += "</script>";
-    html += "</body></html>";
-    request->send(200, "text/html", html);
-});
+    
+    
+    
+   html += "<script>";
+html += "var ws = new WebSocket('ws://' + window.location.hostname + '/ws');";
+html += "ws.onopen = function(event) {";
+html += "  console.log('Conexão WebSocket estabelecida');";
+html += "};";
+html += "function setLight(hour) {";
+html += "  console.log('Botão pressionado para o horário:', hour);";
+html += "  var message = { selectedHour: hour };";
+html += "  ws.send(JSON.stringify(message));";
+html += "}";
+html += "function toggleOnOff(checkbox) {";
+html += "  var onOffValue = checkbox.checked ? 1 : 0;";
+html += "  var message = { onOffState: onOffValue };";
+html += "  ws.send(JSON.stringify(message));";
+html += "}";
+html += "function toggleModoAutomatico(checkbox) {";
+html += "  var modoAutomaticoValue = checkbox.checked ? 1 : 0;";
+html += "  var message = { modoAutomaticoState: modoAutomaticoValue };";
+html += "  ws.send(JSON.stringify(message));";
+html += "}";
 
+// Script para atualizar os switches com base nas variáveis globais
+html += "document.addEventListener('DOMContentLoaded', function() {";
+html += "  var onOffSwitch = document.getElementById('onOffSwitch');";
+html += "  var modoAutomaticoSwitch = document.getElementById('modoAutomaticoSwitch');";
+html += "  onOffSwitch.checked = " + String(onOffState ? "true" : "false") + ";";
+html += "  modoAutomaticoSwitch.checked = " + String(modoAutomaticoState ? "true" : "false") + ";";
+html += "});";
 
+html += "</script>";
 
-  // Adicione o endpoint para obter o estado atual
-  server.on("/getState", HTTP_GET, [](AsyncWebServerRequest *request) {
-    String state = "{\"time\":\"" + timeClient.getFormattedTime() + "\"}";
-    request->send(200, "application/json", state);
-  });
+html += "</body></html>";
+request->send(200, "text/html", html);
 
-  server.begin();
 }
 
-void loop() {
-  // Atualize o cliente NTP
-  timeClient.update();
 
-  // Adicione a lógica do ciclo circadiano aqui
-  // Adicione outras lógicas necessárias para o ciclo circadiano
+void setup() {
+  WiFi.setHostname("luminaria-elements");
+  Serial.begin(115200);
+  pinMode(PWM_OUT_PIN, OUTPUT);
+  pinMode(PWM_OUT_PIN_2, OUTPUT);
+  ledcAttachPin(PWM_OUT_PIN, 0);
+  ledcAttachPin(PWM_OUT_PIN_2, 1);
+  ledcSetup(0, 12000, 10);
+  ledcSetup(1, 12000, 10);
+  
+  connectToWiFi();
+
+  // Configuração do WebSocket
+  ws.onEvent(handleWebSocketMessage);
+  server.addHandler(&ws);
+
+  // Inicia o servidor
+  server.begin();
+
+  // Inicia o cliente NTP
+  timeClient.begin();
+  timeClient.setTimeOffset(-10800); // Horário de Brasília (UTC -3)
+
+  // Adiciona o handler para /globals.js
+  server.on("/globals.js", HTTP_GET, [](AsyncWebServerRequest *request) {
+    // Envie as variáveis globais JavaScript para o cliente
+    String response = "var onOffState = " + String(onOffState) + ";\n";
+    response += "var modoAutomaticoState = " + String(modoAutomaticoState) + ";\n";
+    response += "var selectedHour = " + String(selectedHour) + ";";
+    request->send(200, "text/javascript", response);
+  });
+
+  // Handler para a raiz da página
+  server.on("/", HTTP_GET, handleRoot);
+}
+
+
+void loop() {
+
+   // Verifica se as variáveis foram alteradas
+  static bool prevOnOffState = false;
+  static bool prevModoAutomaticoState = false;
+
+  if (onOffState != prevOnOffState || modoAutomaticoState != prevModoAutomaticoState) {
+    // Atualiza as variáveis de estado anterior
+    prevOnOffState = onOffState;
+    prevModoAutomaticoState = modoAutomaticoState;
+
+    // Cria e envia a mensagem WebSocket para informar o cliente sobre a mudança
+    DynamicJsonDocument jsonDoc(128);
+    jsonDoc["onOffState"] = onOffState;
+    jsonDoc["modoAutomaticoState"] = modoAutomaticoState;
+    String jsonData;
+    serializeJson(jsonDoc, jsonData);
+    ws.textAll(jsonData);
+  }
+  
+  if (onOffState) {
+    pwmValue = 255;
+  } else {
+    pwmValue = 0;
+    pwmValue2 = 0;
+  }
+
+  if (modoAutomaticoState) {
+    pwmValue2 = 255;
+  } else {
+    pwmValue = 0;
+    pwmValue2 = 0;
+  }
+  actionLed();
+   //Serial.print("ON/OFF State: " + String(onOffState));
+  //Serial.print("  -  Modo Automático State: " + String(modoAutomaticoState));
+  //Serial.println(" -  Selected Hour: " + String(selectedHour));
 }
